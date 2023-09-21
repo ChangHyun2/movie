56989f0596f6a00e1851a231cef0fa485ee0302e
changhyun <jchangh2@loplat.com>
2023-09-21 23:09:26 +0900

    feat crawl movie details done

--- data/omdb/createMovieDetail.js
+++ data/omdb/createMovieDetail.js
@@ -1,66 +1,64 @@
-const { readdir, writeFileSync, readFileSync } = require("fs");
+const { readdir, writeFileSync, readFileSync, readdirSync } = require("fs");
 const fetch = require("node-fetch");
 const path = require("path");
 const qs = require("qs");
 
 const failures = new Set();
+const movieDetails = JSON.parse(
+  readFileSync(path.join(__dirname, "./movieDetails.json"))
+);
+const stack = [];
 
-readdir(path.join(__dirname, "../chatgpt/citiesmovies"), (err, files) => {
-  const movieDetails = JSON.parse(
-    readFileSync(path.join(__dirname, "./movieDetails.json"))
-  );
-  const fileTasks = files;
-  const stack = [];
+const files = readdirSync(path.join(__dirname, "../chatgpt/citiesmovies"));
+files.forEach((file) => {
+  const cityMovies = require(path.join(
+    __dirname,
+    "../chatgpt/citiesmovies/" + file
+  ));
 
-  fileTasks.forEach((file) => {
-    const cityMovies = require(path.join(
-      __dirname,
-      "../chatgpt/citiesmovies/" + file
-    ));
+  stack.push(...cityMovies.movies);
+});
 
-    stack.push(...cityMovies.movies);
-  });
+(async () => {
+  while (stack.length) {
+    const movie = stack.shift();
 
-  (async () => {
-    while (stack.length) {
-      const movie = stack.shift();
+    if (movieDetails[movie.id] && movieDetails[movie.id].Response === "False")
+      continue;
 
-      if (
-        movieDetails[movie.id].Response === "False" ||
-        movieDetails[movie.id] === null
-      ) {
-        // continue;
+    if (!movieDetails[movie.id]) {
+      // continue;
+      console.log(movieDetails[movie.id], movie);
 
-        console.log(movie.id);
+      console.log(movie.id);
 
-        console.log(`collect ${movie.id}`);
-        await new Promise((res) => setTimeout(() => res(), 300));
-        const movieDetail = await getMovie({ t: movie.title, y: movie.year });
-        if (movieDetail !== null) {
-          movieDetails[movie.id] = movieDetail;
-          console.log(`collect success`);
-        } else {
-          if (failures.has(movie.title)) {
-            console.log(`failure again : ${movie.title}`);
-          }
-
-          console.log(`collect failure`);
-          failures.add(movie.title);
-          stack.push(movie);
+      console.log(`collect ${movie.id}`);
+      await new Promise((res) => setTimeout(() => res(), 400));
+      const movieDetail = await getMovie({ t: movie.title, y: movie.year });
+      if (movieDetail !== null) {
+        movieDetails[movie.id] = movieDetail;
+        console.log(`collect success`);
+      } else {
+        if (failures.has(movie.title)) {
+          console.log(`failure again : ${movie.title}`);
         }
 
-        writeFileSync(
-          path.join(__dirname, "./movieDetails.json"),
-          JSON.stringify(movieDetails)
-        );
+        console.log(`collect failure`);
+        failures.add(movie.title);
+        stack.push(movie);
       }
+
+      writeFileSync(
+        path.join(__dirname, "./movieDetails.json"),
+        JSON.stringify(movieDetails)
+      );
     }
-  })();
-});
+  }
+})();
 
 const getMovie = async (params) => {
   const _params = {
-    apikey: "ff8a33e4",
+    apikey: "a6e42c0b",
     type: "movie",
     plot: "full",
     r: "json",
